eaa9bdcc0fd97339e6f51679eeddf32997ef3c8a
BarbuLaura <117304669+BarbuLaura@users.noreply.github.com>
2022-11-11 22:00:52 +0200

    Add files via upload

--- spanzuratoarea/script.js
+++ spanzuratoarea/script.js
@@ -9,62 +9,58 @@ var word = document.getElementById("toBeGuessed").value;
 
 function newGame(){
 
-++nrGame;
+  const canvas = document.getElementById("spanzuratoarea");
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-const canvas = document.getElementById("spanzuratoarea");
-const ctx = canvas.getContext('2d');
+  var word = document.getElementById("toBeGuessed").value;
 
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-var word = document.getElementById("toBeGuessed").value;
+  document.getElementById("toBeGuessed").value = null;
+  document.getElementById("toBeGuessed").style.display = "";
 
-document.getElementById("toBeGuessed").value = null;
-document.getElementById("toBeGuessed").style.display = "";
-
-
-
-ctx.strokeStyle = 'brown';
-ctx.lineWidth = 10;
-ctx.beginPath();
-ctx.moveTo(700, 200);
-ctx.lineTo(800, 200);
-ctx.moveTo(750, 200);
-ctx.lineTo(750, 5);
-ctx.lineTo(835,5);
-ctx.lineTo(835, 30);
-ctx.stroke();
+  ctx.strokeStyle = 'brown';
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(700, 200);
+  ctx.lineTo(800, 200);
+  ctx.moveTo(750, 200);
+  ctx.lineTo(750, 5);
+  ctx.lineTo(835,5);
+  ctx.lineTo(835, 30);
+  ctx.stroke();
 }
 
 function addWord(){
-    var word = document.getElementById("toBeGuessed").value;
-    let y = 10;
-    let size = word.length;
+  var word = document.getElementById("toBeGuessed").value;
+  let y = 10;
+  let size = word.length;
 
-    document.getElementById("toBeGuessed").style.display = "none";
+  document.getElementById("toBeGuessed").style.display = "none";
 
-    while(size > 0){
-      drawLines(y);
-      y += 50;
-      --size;
-    }
+  while(size > 0){
+    drawLines(y);
+    y += 50;
+    --size;
+  }
 }
 
 function drawLines(x){
-      ctx.strokeStyle = 'brown';
-      ctx.lineWidth = 10;
-      ctx.beginPath();
-      ctx.moveTo(x, 420);
-      ctx.lineTo(x + 30, 420);
-      ctx.stroke();
+  ctx.strokeStyle = 'brown';
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(x, 420);
+  ctx.lineTo(x + 30, 420);
+  ctx.stroke();
 }
 
 function addLetter(){
- let letter = document.getElementById("letterToBeGuessed").value;
- let size1 = 0;
- let y = 10;
- let guessed = 0
- let word = document.getElementById("toBeGuessed").value;
+  let letter = document.getElementById("letterToBeGuessed").value;
+  let size1 = 0;
+  let y = 10;
+  let guessed = 0
+  let word = document.getElementById("toBeGuessed").value;
 
- while(size1 < word.length){
+  while(size1 < word.length){
     if(letter === word[size1]){
       document.getElementById("nextLetter").innerHTML = "nailed it";
       ctx.fillStyle = 'red';
@@ -75,54 +71,54 @@ function addLetter(){
     }
     y += 50;
     ++size1;
- }
- if(guessed1 === word.length && guessed1 !== 0 ){
-  ctx.fillStyle = 'green';
-  ctx.font = '48px serif';
-  ctx.fillText('ALIVE AND WINNER', 450, 250, 150);
-  guessed1 = 0;
-  missed = 0;
-  document.getElementById("nextLetter").innerHTML = "";
-}
- if(guessed === 0){
-  ++missed;
-  document.getElementById("nextLetter").innerHTML = "drawing..."
-  drawHangman();
-}
-document.getElementById("letterToBeGuessed").value = null;
+  }
+  if(guessed1 === word.length && guessed1 !== 0 ){
+    ctx.fillStyle = 'green';
+    ctx.font = '48px serif';
+    ctx.fillText('ALIVE AND WINNER', 450, 250, 150);
+    guessed1 = 0;
+    missed = 0;
+    document.getElementById("nextLetter").innerHTML = "";
+  }
+  if(guessed === 0){
+    ++missed;
+    document.getElementById("nextLetter").innerHTML = "drawing..."
+    drawHangman();
+  }
+  document.getElementById("letterToBeGuessed").value = null;
 }
 
 function drawHangman(){
   let word = document.getElementById("toBeGuessed").value;
 
- if(missed === word.length){
+  if(missed === word.length){
     drawTheHangMan();
     missed = 0;
     guessed1 = 0;
     document.getElementById("nextLetter").innerHTML = "";
-  }else{
-  if(missed === 1 ){
-    drawHead();
-  }
-  if (missed === 2){    
-    drawBody();
-  }
-  if(missed === 3){ 
-    drawLeftHand();
+  } else {
+    if(missed === 1 ){
+      drawHead();
+    }
+    if (missed === 2){    
+      drawBody();
+    }
+    if(missed === 3){ 
+      drawLeftHand();
+    } 
+    if(missed === 4){
+      drawRightHand();
+    }    
+    if(missed === 5){
+      drawLeftFoot();
+    }
+    if(missed === 6 || missed === word.length){ 
+      drawRightFoot();
+      missed = 0;
+      guessed1 = 0;
+      document.getElementById("nextLetter").innerHTML = "";
+    }
   } 
-  if(missed === 4){
-    drawRightHand();
-  }    
-  if(missed === 5){
-    drawLeftFoot();
-  }
-  if(missed === 6 || missed === word.length){ 
-    drawRightFoot();
-    missed = 0;
-    guessed1 = 0;
-    document.getElementById("nextLetter").innerHTML = "";
-  }
-} 
 }
 
 
@@ -132,6 +128,7 @@ function drawHead(){
   ctx.arc(835, 50, 20, 0, Math.PI*2, true);
   ctx.stroke();
 }
+
 function drawBody(){  
   ctx.strokeStyle = 'brown';
   ctx.lineWidth = 10;  
@@ -140,6 +137,7 @@ function drawBody(){
   ctx.lineTo(835, 150);
   ctx.stroke();
 }
+
 function drawLeftHand(){
   ctx.strokeStyle = 'brown';
   ctx.lineWidth = 10; 
@@ -147,7 +145,8 @@ function drawLeftHand(){
   ctx.moveTo(835, 90);
   ctx.lineTo(815, 120);
   ctx.stroke();
-} 
+}
+
 function drawRightHand(){
   ctx.strokeStyle = 'brown';
   ctx.lineWidth = 10; 
@@ -155,7 +154,8 @@ function drawRightHand(){
   ctx.moveTo(835, 90);
   ctx.lineTo(855, 120);
   ctx.stroke();
-}    
+} 
+
 function drawLeftFoot(){
   ctx.strokeStyle = 'brown';
   ctx.lineWidth = 10; 
@@ -164,6 +164,7 @@ function drawLeftFoot(){
   ctx.lineTo(815, 169);
   ctx.stroke();
 }
+
 function drawRightFoot(){ 
   ctx.strokeStyle = 'brown';
   ctx.lineWidth = 10;
